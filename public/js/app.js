731ce4f6510cb31bd6cdc004ae5ca828d6a1b5e0
raviprasad786 <raviprasad@vgsconsultancy.com>
2016-09-09 19:58:08 +0530

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+angular.module("contactsApp", ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "list.html",
+                controller: "ListController",
+                resolve: {
+                    contacts: function(Contacts) {
+                        return Contacts.getContacts();
+                    }
+                }
+            })
+            .when("/new/contact", {
+                controller: "NewContactController",
+                templateUrl: "contact-form.html"
+            })
+            .when("/contact/:contactId", {
+                controller: "EditContactController",
+                templateUrl: "contact.html"
+            })
+            .otherwise({
+                redirectTo: "/"
+            })
+    });
---
+++ public/js/controllerApp.js
@@ -0,0 +1,45 @@
+angular.module("contactsApp")    
+    .controller("ListController", function(contacts, $scope) {
+        $scope.contacts = contacts.data;
+    })
+    .controller("NewContactController", function($scope, $location, Contacts) {
+        $scope.back = function() {
+            $location.path("#/");
+        }
+
+        $scope.saveContact = function(contact) {
+            Contacts.createContact(contact).then(function(doc) {
+                var contactUrl = "/contact/" + doc.data._id;
+                $location.path(contactUrl);
+            }, function(response) {
+                alert(response);
+            });
+        }
+    })
+    .controller("EditContactController", function($scope, $routeParams, Contacts) {
+        Contacts.getContact($routeParams.contactId).then(function(doc) {
+            $scope.contact = doc.data;
+        }, function(response) {
+            alert(response);
+        });
+
+        $scope.toggleEdit = function() {
+            $scope.editMode = true;
+            $scope.contactFormUrl = "contact-form.html";
+        }
+
+        $scope.back = function() {
+            $scope.editMode = false;
+            $scope.contactFormUrl = "";
+        }
+
+        $scope.saveContact = function(contact) {
+            Contacts.editContact(contact);
+            $scope.editMode = false;
+            $scope.contactFormUrl = "";
+        }
+
+        $scope.deleteContact = function(contactId) {
+            Contacts.deleteContact(contactId);
+        }
+    });
